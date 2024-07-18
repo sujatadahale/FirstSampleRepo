2d44d58abcff46fa210c86028e0535de6572f433
sujatadahale <sdahale84@gmail.com>
2024-07-17 20:53:41 -0400

    First Version

--- main.py
+++ main.py
@@ -5,11 +5,12 @@
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
+    # Use a vbreakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def myFirstApp():
     print("Hello World")
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
